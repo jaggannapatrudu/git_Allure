6e31f291ebe18ab69a167132bad0ca9da6a8595a
Karri <jaggannapatrudu.karri@bms.com>
2025-12-31 12:14:56 +0530

    commit

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -1,18 +1,9 @@
 
 
 import { test, expect} from '@playwright/test'
-import fs from 'fs'
-let payload = {
-  /*  "name": "Nihanya mobile",
-   "data": {
-      "year": 2023,
-      "price": 312323,
-      "CPU model": "Nihanya icore",
-      "Hard disk size": "2000 TB" */
-   //}
-}
+import path from 'path'
 test('api calls', async({request})=>{
-
+let testss = path.join(__dirname, 'orangeHRM_Locators/OrangeHRM_Locators.json')
 
     
 })
